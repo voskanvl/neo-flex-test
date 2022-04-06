fcc64efd914e7667a7c54ba59d736aa86b108918
voskanvl <voskanvl@gmail.com>
2022-04-06 14:56:37 +0300

    counter in basket card

---
+++ src/components/basket/Counter/Counter.jsx
@@ -0,0 +1,49 @@
+import style from "./Counter.module.sass";
+export const Counter = ({ value, onChange }) => {
+    return (
+        <div className={style.counter}>
+            <button
+                className={style.counter__button}
+                onClick={() => onChange(-1)}>
+                <svg
+                    width="36"
+                    height="30"
+                    viewBox="0 0 36 30"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <ellipse
+                        cx="17.9945"
+                        cy="15"
+                        rx="17.5833"
+                        ry="15"
+                        fill="#FFCE7F"
+                    />
+                    <path d="M9.78888 14H26.2V16H9.78888V14Z" fill="white" />
+                </svg>
+            </button>
+            <div className={style.counter__value}>{value}</div>
+            <button
+                className={style.counter__button}
+                onClick={() => onChange(1)}>
+                <svg
+                    width="36"
+                    height="30"
+                    viewBox="0 0 36 30"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <ellipse
+                        cx="18.2559"
+                        cy="15"
+                        rx="17.5833"
+                        ry="15"
+                        fill="#FFCE7F"
+                    />
+                    <path
+                        d="M17.0837 14V8H19.4282V14H26.4615V16H19.4282V22H17.0837V16H10.0504V14H17.0837Z"
+                        fill="white"
+                    />
+                </svg>
+            </button>
+        </div>
+    );
+};
