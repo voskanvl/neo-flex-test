167165ce42d89ccbf4ade91bdb566ab125476519
voskanvl <voskanvl@gmail.com>
2022-04-06 14:18:59 +0300

    refactor basket store

--- src/effector/basket.effector.js
+++ src/effector/basket.effector.js
@@ -2,15 +2,46 @@ import { createStore, createEffect, restore, createEvent } from "effector";
 import { data } from "../data";
 
 export const show = createEvent("show");
+
+export const $isVisible = restore(show, true);
+/*
+{
+    id:number,
+    product:{id:number, price: number ....},
+    volume: number
+}
+*/
 export const addProductFx = createEffect(async id => {
     return data.headphones.filter(headphone => headphone.id === id);
 });
 
 addProductFx.done.watch(e => console.log("addProductFx", e));
 
-export const $isVisible = restore(show, true);
-export const $basket = createStore([]).on(addProductFx.done, (state, r) => [
-    ...state,
-    r.result[0],
-]);
-$basket.watch(e => console.log("$backet", e));
+export const $basket = createStore([]).on(addProductFx.done, (state, res) => {
+    const hasInState = state.findIndex(e => e.id === res.params);
+    if (hasInState !== -1) {
+        const newState = [...state];
+        newState.splice(hasInState, 1);
+        return [
+            ...newState,
+            {
+                id: res.params,
+                product: res.result[0],
+                volume: state[hasInState]["volume"] + 1,
+            },
+        ];
+    }
+    return [
+        ...state,
+        {
+            id: res.params,
+            product: res.result[0],
+            volume: 1,
+        },
+    ];
+});
+
+$basket.watch(e => {
+    console.log("$basket", e);
+    if (e.length > 1) console.log(Object.is(...e));
+});
