ed650f9c6b5ae123bea4b56e15adad065daf9984
Anton Voskanyan <voskanvl@gmail.com>
2022-04-06 06:09:01 +0300

    fix svg properties

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -22,37 +22,37 @@ export const Footer = () => {
                             <path
                                 d="M1.66666 9.99996C1.66666 14.6025 5.3975 18.3333 10 18.3333C14.6025 18.3333 18.3333 14.6025 18.3333 9.99996C18.3333 5.39746 14.6025 1.66663 10 1.66663C5.3975 1.66663 1.66666 5.39746 1.66666 9.99996Z"
                                 stroke="#838383"
-                                stroke-width="1.5"
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
+                                strokeWidth="1.5"
+                                strokeWinecap="round"
+                                strokeWinejoin="round"
                             />
                             <path
                                 d="M10.8333 1.70825C10.8333 1.70825 13.3333 4.99992 13.3333 9.99992C13.3333 14.9999 10.8333 18.2916 10.8333 18.2916"
                                 stroke="#838383"
-                                stroke-width="1.5"
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
+                                strokeWidth="1.5"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
                             />
                             <path
                                 d="M9.16666 18.2916C9.16666 18.2916 6.66666 14.9999 6.66666 9.99992C6.66666 4.99992 9.16666 1.70825 9.16666 1.70825"
                                 stroke="#838383"
-                                stroke-width="1.5"
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
+                                strokeWidth="1.5"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
                             />
                             <path
                                 d="M2.19167 12.9166H17.8083"
                                 stroke="#838383"
-                                stroke-width="1.5"
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
+                                strokeWidth="1.5"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
                             />
                             <path
                                 d="M2.19167 7.08325H17.8083"
                                 stroke="#838383"
-                                stroke-width="1.5"
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
+                                strokeWidth="1.5"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
                             />
                         </svg>
                         <a href="#" className="red">
