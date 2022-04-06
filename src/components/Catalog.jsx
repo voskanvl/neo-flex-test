76ea675f257203bd58dd4f4d2205a2482f8307a0
Anton Voskanyan <voskanvl@gmail.com>
2022-04-06 08:17:31 +0300

    replace in Catalog

---
+++ src/components/Catalog.jsx
@@ -0,0 +1,25 @@
+import { Card } from "./Card";
+import { useStore } from "effector-react";
+import { data } from "../effector/";
+import { Group } from "./Group";
+export const Catalog = () => {
+    const headphones = useStore(data.$data);
+    return (
+        <>
+            <Group title={"наушники"}>
+                {headphones
+                    .filter(headphone => !headphone.wireless)
+                    .map(headphone => (
+                        <Card {...headphone} key={headphone.id} />
+                    ))}
+            </Group>
+            <Group title={"Беспроводные наушники"}>
+                {headphones
+                    .filter(headphone => headphone.wireless)
+                    .map(headphone => (
+                        <Card {...headphone} key={headphone.id} />
+                    ))}
+            </Group>
+        </>
+    );
+};
