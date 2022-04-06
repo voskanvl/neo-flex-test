7bce9823339b9d8bd2f5749b4586fb2f1ee8f7f1
voskanvl <voskanvl@gmail.com>
2022-04-06 17:47:48 +0300

    fix index effector

--- src/effector/index.js
+++ src/effector/index.js
@@ -1,5 +1,17 @@
 import { $data, getDataFx } from "./data.effector";
-import { $isVisible, show, $basket, addProductFx } from "./basket.effector";
+import {
+    $isVisible,
+    show,
+    $basket,
+    addProductFx,
+    removetProduct,
+} from "./basket.effector";
 
 export const data = { $data, getDataFx };
-export const basket = { $isVisible, show, $basket, addProductFx };
+export const basket = {
+    $isVisible,
+    show,
+    $basket,
+    addProductFx,
+    removetProduct,
+};
